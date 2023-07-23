56576347514057eab564e6061312256260712e99
dannibove <bovedaniel@hotmail.com>
2023-07-23 20:43:55 -0300

    Inicio

---
+++ JS/Probando.js
@@ -0,0 +1,9 @@
+console.log('Hola');
+
+var a = 10
+var b = 10
+console.log(`¡JavaScript se publicó hace ${a+b} años!`)
+// resultado => ¡JavaScript se publicó hace 20 años!
+console.log(`Existen ${2 * (a + b)} frameworks JavaScript y no ${10 * (a + b)}.`)
+// Existen 40 frameworks JavaScript y no 200.
+console.log(a)
---
+++ README.md
@@ -0,0 +1,1 @@
+# El-Bosqucito-de-Tanti
